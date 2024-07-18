888426004514d0d09f9c2313e85c1a602dac69fd
SUNILSSE <129818370+SUNILSSE@users.noreply.github.com>
2024-07-18 13:21:50 +0530

    Add files via upload

---
+++ day-1, 03.distribution of diff products.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data={
+    'category':['category a','category b','category c'],
+    'sales':[10000,15000,20000]
+    }
+df=pd.DataFrame(data)
+print("data:")
+print(df)
+plt.figure(figsize=(10,6))
+plt.plot(df['category'], df['sales'], marker='o',linestyle='-',color='b')
+plt.title('sales distribtion across product categories')
+plt.xlabel('product category')
+plt.ylabel('sales')
+plt.grid(True)
+plt.show()
+plt.figure(figsize=(10,6))
+plt.scatter(df['category'], df['sales'],color='g', s=100)
+plt.title('sale sdistribution across product categories ')
+plt.xlabel('product category')
+plt.ylabel('sales')
+plt.grid(True)
+plt.show()
+plt.figure(figsize=(10,6))
+plt.bar(df['category'], df['sales'], color='c')
+plt.title('Sales Distribution Across Product Categories')
+plt.xlabel('Product Category')
+plt.ylabel('Sales')
+plt.grid(True)
+plt.show()
+
